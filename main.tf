0c2b0d8ab1879bd196f1ca6a756e3552ddcc5419
Jayraj <jayraj.miniyar@gmail.com>
2021-11-02 20:57:24 +0530

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops", "Billable"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
